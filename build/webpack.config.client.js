0479165dc06b9515a96622092247a8125fd9b966
liangshaojie <877942720@qq.com>
2018-03-25 22:03:37 +0800

    初始化项目

---
+++ build/webpack.config.client.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
+
+const baseConfig = require('./webpack.config.base.js')
+const merge = require('webpack-merge')
+const defaultPluins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: isDev ? '"development"' : '"production"'
+        }
+    }),
+    new HTMLPlugin()
+]
+
+
+const devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+        errors: true,
+    },
+    hot: true
+}
+let config;
+if (isDev) {
+    config = merge(baseConfig,{
+        devServer,
+        devtool:'#cheap-module-eval-source-map',
+        module: {
+            rules:[{
+                test: /\.styl/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    'stylus-loader'
+                ]
+            }],
+        },
+        plugins:defaultPluins.concat([
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NoEmitOnErrorsPlugin()
+        ])
+    })
+} else {
+    config = merge(baseConfig,{
+        entry:{
+            app: path.join(__dirname, '../client/index.js'),
+            vendor: ['vue']
+        },
+        output:{
+            filename:'[name].[chunkhash:8].js'
+        },
+        module:{
+            rules:[
+                {
+                    test: /\.styl/,
+                    use: ExtractPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            {
+                                loader: 'postcss-loader',
+                                options: {
+                                    sourceMap: true,
+                                }
+                            },
+                            'stylus-loader'
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins:defaultPluins.concat([
+            new ExtractPlugin('styles.[contentHash:8].css'),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor'
+            }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'runtime'
+            })
+        ])
+    })
+}
+
+module.exports = config
