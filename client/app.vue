1939caa99ababdca6dd2b100f3cdfce5c8968ee8
877942720@qq.com <lsj890305>
2018-03-29 10:13:04 +0800

    4-5

--- client/app.vue
+++ client/app.vue
@@ -1,46 +1,58 @@
 <template>
-  <div id="app">
-    <div id="cover"></div>
-    <Header></Header>
-    <!--<todo></todo>-->
-    <router-link to="/app">app</router-link>
-    <router-link to="/login">login</router-link>
-    <router-view></router-view>
-    <Footer></Footer>
-  </div>
+    <div id="app">
+        <div id="cover"></div>
+        <Header></Header>
+        <p>  {{count}}</p>
+        <!--<todo></todo>-->
+        <router-link to="/app">app</router-link>
+        <router-link to="/login">login</router-link>
+        <router-view></router-view>
+        <Footer></Footer>
+    </div>
 </template>
 <script>
-import Header from './views/layout/header.vue'
-import Footer from './views/layout/footer.jsx'
-import Todo from './views/todo/todo.vue'
+    import Header from './views/layout/header.vue'
+    import Footer from './views/layout/footer.jsx'
+    import Todo from './views/todo/todo.vue'
 
-export default {
-  components: {
-    Header,
-    Footer,
-    Todo
-  }
-}
+    export default {
+        components: {
+            Header,
+            Footer
+        },
+        mounted() {
+            let i = 0
+            setInterval(() => {
+                this.$store.commit("updateCount",++i)
+            },1000)
+        },
+        computed:{
+            count() {
+                return this.$store.state.count
+            }
+        }
+    }
 </script>
 
 <style lang="stylus" scoped>
-#app{
-  position absolute
-  left 0
-  right 0
-  top 0
-  bottom 0
-}
-#cover{
-  position absolute
-  left 0
-  top 0
-  right 0
-  bottom 0
-  background-color #999
-  opacity .9
-  z-index -1
-}
+    #app {
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0
+    }
+
+    #cover {
+        position absolute
+        left 0
+        top 0
+        right 0
+        bottom 0
+        background-color #999
+        opacity .9
+        z-index -1
+    }
 </style>
 
 
--- client/index.js
+++ client/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './app.vue'
+import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import './assets/styles/global.styl'
 import createRouter from './config/router.js'
+import createStore from './store/store.js'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = createRouter()
+const store = createStore()
 new Vue({
+    store,
     router,
     render: (h) => h(App)
 }).$mount('#root')
